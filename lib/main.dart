7975452cbc1a3d664267cbe47fb04f21a45617ba
Anton Nilsson <anton.smeliy@gmail.com>
2024-09-17 04:15:41 +0200

    Updated with full state functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,71 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+class MyState extends ChangeNotifier {
+  final List<ToDoItem> _toDoList = [
+    ToDoItem("Have fun", false),
+    ToDoItem("Have funz", true),
+    ToDoItem("Dance", false),
+    ToDoItem("Do the split", false),
+  ];
+
+  int filter = 1;
+
+  // ignore: prefer_final_fields
+  List<ToDoItem> _toDoListDisplay = [
+    ToDoItem("Have fun", false),
+    ToDoItem("Have funz", true),
+    ToDoItem("Dance", false),
+    ToDoItem("Do the split", false),
+  ];
+
+  List<ToDoItem> get toDoList => _toDoList;
+  List<ToDoItem> get toDoListDisplay => _toDoListDisplay;
+
+  void setFilter(int newFilter) {
+    filter = newFilter;
+    updateDisplay();
+  }
+
+  void add(ToDoItem newItem) {
+    _toDoList.add(newItem);
+    updateDisplay();
+  }
+
+  void remove(ToDoItem itemToRemove) {
+    _toDoList.remove(itemToRemove);
+    updateDisplay();
+  }
+
+  void updateStatus(ToDoItem itemToUpdate) {
+    _toDoList[_toDoList.indexWhere((item) => item == itemToUpdate)]
+            .isItCompleted =
+        !_toDoList[_toDoList.indexWhere((item) => item == itemToUpdate)]
+            .isItCompleted;
+    updateDisplay();
+  }
+
+  void updateDisplay() {
+    switch (filter) {
+      case 1:
+        _toDoListDisplay = _toDoList;
+      case 2:
+        _toDoListDisplay =
+            _toDoList.where((item) => item.isItCompleted == true).toList();
+      case 3:
+        _toDoListDisplay =
+            _toDoList.where((item) => item.isItCompleted == false).toList();
+    }
+    notifyListeners();
+  }
+}
+
 void main() {
-  runApp(const MyApp());
+  MyState state = MyState();
+
+  runApp(
+    ChangeNotifierProvider(create: (context) => state, child: MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -24,13 +87,6 @@ class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
   @override
   Widget build(BuildContext context) {
-    List<ToDoItem> theToDoList = [
-      ToDoItem("Have fun", false),
-      ToDoItem("Have funz", true),
-      ToDoItem("Dance", false),
-      ToDoItem("Do the split", false),
-    ];
-
     return Scaffold(
         appBar: AppBar(
           iconTheme: IconThemeData(color: Colors.black, size: 24),
@@ -39,24 +95,25 @@ class MyHomePage extends StatelessWidget {
           backgroundColor: Colors.deepPurple,
           actions: <Widget>[
             PopupMenuButton(
-                onSelected: (String value) {},
-                itemBuilder: (BuildContext ctx) => [
-                      const PopupMenuItem(value: '1', child: Text('all')),
-                      const PopupMenuItem(value: '2', child: Text('done')),
-                      const PopupMenuItem(value: '3', child: Text('undone')),
+                onSelected: (String value) {
+                  context.read<MyState>().setFilter(int.parse(value));
+                },
+                itemBuilder: (BuildContext context) => [
+                      const PopupMenuItem(value: "1", child: Text('all')),
+                      const PopupMenuItem(value: "2", child: Text('done')),
+                      const PopupMenuItem(value: "3", child: Text('undone')),
                     ])
           ],
         ),
         body: ListView.builder(
           itemBuilder: (context, index) {
-            return GestureDetector(
-              onTap: () {},
-              child: ToDoButton(theToDoList[index]),
-            );
+            return ToDoButton(context.watch<MyState>().toDoListDisplay[index]);
           },
-          itemCount: theToDoList.length,
+          itemCount: context.watch<MyState>().toDoListDisplay.length,
         ),
         floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.deepPurple,
+          foregroundColor: Colors.white,
           onPressed: () {
             Navigator.push(context,
                 MaterialPageRoute(builder: (context) => AddItemToToDoPage()));
@@ -68,7 +125,7 @@ class MyHomePage extends StatelessWidget {
 
 class ToDoItem {
   final String actionItem;
-  final bool isItCompleted;
+  bool isItCompleted;
   ToDoItem(this.actionItem, this.isItCompleted);
 }
 
@@ -86,21 +143,29 @@ class ToDoButton extends StatelessWidget {
             padding: EdgeInsets.all(15),
             child: Checkbox(
               value: event.isItCompleted,
-              onChanged: (value) => value!,
+              onChanged: (value) => context.read<MyState>().updateStatus(event),
             ),
           ),
           Text(
             event.actionItem,
-            style: TextStyle(fontSize: 21),
+            style: TextStyle(
+                fontSize: 21,
+                decoration:
+                    event.isItCompleted ? TextDecoration.lineThrough : null),
           ),
           Expanded(
               child:
                   Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
             Padding(
               padding: EdgeInsets.all(15),
-              child: Icon(
-                Icons.close,
-                size: 32,
+              child: IconButton(
+                onPressed: () {
+                  context.read<MyState>().remove(event);
+                },
+                icon: Icon(
+                  Icons.close,
+                  size: 32,
+                ),
               ),
             ),
           ])),
@@ -110,8 +175,10 @@ class ToDoButton extends StatelessWidget {
 
 class AddItemToToDoPage extends StatelessWidget {
   AddItemToToDoPage({super.key});
+
   @override
   Widget build(BuildContext context) {
+    String newItemToAdd = "";
     return Scaffold(
         appBar: AppBar(
             title: const Text("TIG333 TODO",
@@ -123,12 +190,37 @@ class AddItemToToDoPage extends StatelessWidget {
               padding:
                   EdgeInsets.only(right: 20, left: 20, top: 30, bottom: 30),
               child: TextField(
+                onChanged: (value) {
+                  newItemToAdd = value;
+                },
                 decoration: InputDecoration(
                     border: OutlineInputBorder(), hintText: "Input text here"),
               ),
             ),
             GestureDetector(
-                onTap: () {},
+                onTap: () {
+                  if (newItemToAdd == "") {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: const Text("The text box is empty"),
+                          actions: <Widget>[
+                            TextButton(
+                              child: const Text('Ok'),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  } else {
+                    context.read<MyState>().add(ToDoItem(newItemToAdd, false));
+                    Navigator.pop(context);
+                  }
+                },
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [Icon(Icons.add), Text("ADD")],
